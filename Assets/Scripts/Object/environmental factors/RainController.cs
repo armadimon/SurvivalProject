177e4524e0de0de02aa8d08a1ca7fd460693d938
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-03-19 09:41:02 +0900

    [ Dosc ] 비 사운드 추가 및 스크립트 추가

--- Assets/Scripts/Object/environmental factors/RainController.cs
+++ Assets/Scripts/Object/environmental factors/RainController.cs
@@ -12,6 +12,7 @@ public class RainController : MonoBehaviour
     public ParticleSystem ripplePS;     // 물결 파티클 시스템
 
     private PlayerCondition playerCondition;
+    public AudioSource rainSound;        // 비 소리
 
     private int bossCount = 0;
 
